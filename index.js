fb4f2a8e58bd71cdb9686a99c1801c9fac8f7b7f
raghava20 <raghavaanand22@gmail.com>
2022-02-27 13:02:13 +0530

    done server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import { mongo } from "./connection.js"
+import { authRouter } from "./routes/auth.js"
+import { userRouter } from "./routes/user.js"
+
+dotenv.config()
+const app = express()
+let PORT = 8000 || process.env.PORT
+
+app.use(express.json())
+app.use(cors())
+
+mongo()
+
+app.get('/', (req, res) => {
+    res.send("You are listening on paint servicing api")
+})
+
+app.use("/", authRouter)
+app.use("/", userRouter)
+
+app.listen(PORT, () => console.log('listening on port ', PORT))
